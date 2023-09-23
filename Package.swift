026ce8a1487fbf2eb091a34c510a6dcd1af27689
Rolando Rodríguez <32949104+rorodriguez116@users.noreply.github.com>
2023-09-23 17:09:35 +0100

    Fixed problem with focus circle position re Issue 11. (#12)
    Co-authored-by: Rolando Rodríguez <rorodriguez@icloud.com>

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Camera-SwiftUI",
     platforms: [
         .iOS(.v13),
-        .macCatalyst(.v14)
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
